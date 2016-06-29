eafe52b218a157f55500ba933bec68d75c64d522
tohpam <toh.pam@gmail.com>
2016-06-30 00:48:55 +0900

    Fixed Up Comments

--- src/World.java
+++ src/World.java
@@ -1,54 +1,46 @@
-// MAIN METHOD
+// WORLD (MAP)
 
-// import java.util.ArrayList;
+// Contains the 2D arrays that keep track of agent locations in a virtual map
+// Has a user-defined width and height
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
 public class World {
-    // public static Tile[][] coordinates; // Two-dimensional array
-    public Agent[][] agentMap; // Two-dimensional array
-    // public ArrayList<Tile> theTiles= new ArrayList<>(); // ArrayList to hold tiles in the map
-    public final int width;
-    public final int height;
+    public Agent[][] agentMap; // Two-dimensional array containing Agents
+    public final int width; // x-axis from 0 to width
+    public final int height; // y-axis from 0 to height
 
     // Creates an instance of a map with a user-defined size
-    // Calls the Tile constructor and adds tiles to the map
     public World (int width, int height) {
-//        coordinates = new Tile[width][height];
-//        for(int i = 0; i<width; i++){
-//            for(int j = 0; j<height; j++){
-//                coordinates[i][j] = new Tile(i,j);
-//                theTiles.add(coordinates[i][j]);
-//            }
-//        }
         this.width = width;
         this.height = height;
-        this.agentMap = new Agent[width][height];
+        this.agentMap = new Agent[width][height]; // 2D Array gets its predetermined size
     }
 
-    // A Person is placed on a Tile
+    // Places an Agent (usually Person) in a position in the 2D array
     public void placeAgent(Agent a){
-        int x,y;
+        int x,y; // x for horizontal, y for vertical
         
         Random rand = new Random();
                 
-    	do{
+    	do{ // Try a random x and y
         	x = rand.nextInt(width);
         	y = rand.nextInt(height);
-        }while(agentMap[x][y] != null);
+        }while(agentMap[x][y] != null); // If the space in (x,y) is taken, try again
     	
-    	// System.out.println("x, y " + x + ", " + y);
+//    	System.out.println("x, y " + x + ", " + y); // Tester
     	
-    	agentMap[x][y] = a;
+    	agentMap[x][y] = a; // Puts Agent in the empty (x,y) slot
     	
+    	// Updates Agent's coordinates
     	a.xLocation = x;
     	a.yLocation = y;
     	
-    	//System.out.println("x, y " + a.xLocation + ", " + a.yLocation);
+//    	System.out.println("x, y " + a.xLocation + ", " + a.yLocation); // Tester
     	
-        // coordinates[x][y].addPerson(p);
-    } // how to clear array?
+    }
     
     public List<Agent> findNearbyAgents(int x, int y){
     	List<Agent> neighbors = new LinkedList<Agent>();
@@ -71,7 +63,28 @@ public class World {
     public void removeAgent (Agent a){
     	// fancy comment
     }
+}
+
+    // UNUSED STUFF
     
+    // public static Tile[][] coordinates; // Two-dimensional array
+    // public ArrayList<Tile> theTiles= new ArrayList<>(); // ArrayList to hold tiles in the map
+    // Creates an instance of a map with a user-defined size
+    // Calls the Tile constructor and adds tiles to the map
+    
+// Calls the Tile constructor and adds tiles to the map
+//    public World (int width, int height) {
+//      coordinates = new Tile[width][height];
+//      for(int i = 0; i<width; i++){
+//          for(int j = 0; j<height; j++){
+//              coordinates[i][j] = new Tile(i,j);
+//              theTiles.add(coordinates[i][j]);
+//          }
+//      }
+//    }
+
+// coordinates[x][y].addPerson(p);
+
     // A Person is moved from the Tile
 //	public static void movePerson (Person p){
 //		int x = p.getX();
@@ -79,5 +92,3 @@ public class World {
 //		
 //		coordinates[x][y].removePerson(p);
 //	}
-
-}
